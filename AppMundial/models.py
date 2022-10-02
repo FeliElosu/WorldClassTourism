94404c03c6e2bf07f05d336d44979b12a8b08bee
Guillermo Acuña <guillermo.oscar.acuna@gmail.com>
2022-10-02 13:00:34 -0300

    se agregan permisos para que que aparezcan los botones de edicion de posts

--- AppMundial/models.py
+++ AppMundial/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from ckeditor.fields import RichTextField
+from django.contrib.auth.models import User
 
 #Clase para el posteo en Blog
 class Posts(models.Model):
@@ -11,7 +12,7 @@ class Posts(models.Model):
     #Caratula de Inicio
     creado = models.DateTimeField(auto_now_add=True)
     actualizado = models.DateTimeField(auto_now=True)
-    autor = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,)
+    autor = models.ForeignKey(User,on_delete=models.CASCADE)
     image = models.ImageField(upload_to='thumbnail/', null=True, blank=True)
     
     class Meta: 
